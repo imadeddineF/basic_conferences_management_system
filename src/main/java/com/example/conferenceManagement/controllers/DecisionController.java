de74fc0127c7ea2cbbbf116e9623577876097bed
imadeddineF <imadeddinefillalipro@gmail.com>
2024-12-27 14:59:29 +0100

    implement decision controller endpoint & service

--- src/main/java/com/example/conferenceManagement/controllers/DecisionController.java
+++ src/main/java/com/example/conferenceManagement/controllers/DecisionController.java
@@ -1,8 +1,26 @@
 package com.example.conferenceManagement.controllers;
 
+import com.example.conferenceManagement.entities.Conference;
+import com.example.conferenceManagement.enums.ESubmissionStatus;
+import com.example.conferenceManagement.services.interfaces.DecisionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/api")
 public class DecisionController {
-  
+    private DecisionService decisionService;
+
+    @Autowired
+    public DecisionController(DecisionService decisionService) {
+        this.decisionService = decisionService;
+    }
+
+    @GetMapping("/conferences/decision/{conferenceId}")
+    public ESubmissionStatus getConferenceDecisionById(@PathVariable Long conferenceId) {
+        return decisionService.findConferenceDecisionById(conferenceId);
+    }
 }
--- src/main/java/com/example/conferenceManagement/services/interfaces/DecisionService.java
+++ src/main/java/com/example/conferenceManagement/services/interfaces/DecisionService.java
@@ -1,5 +1,8 @@
 package com.example.conferenceManagement.services.interfaces;
 
+import com.example.conferenceManagement.entities.Conference;
+import com.example.conferenceManagement.enums.ESubmissionStatus;
+
 public interface DecisionService {
-  
+    ESubmissionStatus findConferenceDecisionById(Long conferenceId);
 }
